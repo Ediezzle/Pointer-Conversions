2ce54dbf0492a6491b34a3dec5d17401af8210ce
Ediezzle <edmore.mandikiyana@gmail.com>
2020-01-13 10:31:49 +0200

    Add project files.

---
+++ PointerConversions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PointerConversions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
